bc3eb5d06ec81aa988f38710f714fe68bf8262a8
Drew <theusefulmonk@gmail.com>
2026-05-01 07:59:49 -0500

    Establish functionality for legible printing, reading from stdin or filename, and node deletion.

--- tweak_structure.py
+++ tweak_structure.py
@@ -8,6 +8,7 @@
 
 import sys
 import os
+import stat
 import json
 from pprint import pprint
 from shutil import get_terminal_size
@@ -45,7 +46,12 @@ def keep_node(n):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename')
+parser.add_argument('filename',
+                    type=str,
+                    #type=argparse.FileType('r'),
+                    nargs='?',
+                    default='-'
+                    )
 parser.add_argument('-d',
                     '--delete',
                     type=int,
@@ -64,61 +70,89 @@ parser.add_argument('-n',
                     action='store_true')
 args = parser.parse_args()
 
-# Inputs state
-source = args.filename
-should_print = True #Default
+# Check inputs
+if stdin_is_piped():
+    print("INFO: The tweak script is reading from stdin.")
+    program_input = sys.stdin.read()
+    #print(program_input)
+    #with standard_input:
+    #    pipe_input = standard_input.read()
+else: 
+    # The script has been invoked standalone, not in a pipeline.
+   cache = shelve.open('.toc_cache')
+   try:
+       # Convert to string so we don't have to worry about how program_input was loaded
+       program_input = json.dumps(cache[args.filename], ensure_ascii=False)
+       print("INFO: No input received on stdin; loading from cache.")
+       #print(program_input)
+       cache.close()
+   except KeyError:
+       cache.close()
+       print("ERROR: No toc for the specified file is found in the cache.")
+       sys.exit()
+# Flags state
+should_print = True # Default
 if args.noprint:
     should_print = False
 if args.delete:
+    should_print = False
     nodes_to_delete = [(lambda x: x - 1)(x) for x in args.delete]
-    print("Indices of nodes to delete are: " + str(list(nodes_to_delete)))
+    print("INFO: Indices of nodes to delete are: " + str(list(nodes_to_delete)))
     #print(str(list(nodes_to_delete)))
 else:
     pass
 if args.update:
+    should_print = False
     nodes_to_update = [lambda x: x - 1 for x in args.update]
 else:
     pass
 
-# Check inputs
-if stdin_is_piped():
-    pipe_input = sys.stdin.read()
-else: 
-   cache = shelve.open('.toc_cache')
-
-   try:
-       pipe_input = cache[source]
-       print("No input received on stdin; loading from cache")
-       cache.close()
-   except KeyError:
-       cache.close()
-       print("no toc for the specified file is found in the cache.")
-       sys.exit()
 
 # Program state
-if isinstance(pipe_input, str):
-    json_representation = json.loads(str(pipe_input))
-elif isinstance(pipe_input, dict):
-    json_representation = pipe_input
+#if isinstance(program_input, str):
+#    #print("piped input is " + program_input)
+#    json_representation = json.loads(program_input)
+#    print("loaded from standard input.")
+#elif isinstance(program_input, dict):
+#    json_representation = program_input
+#    print("No filename given. Loaded from cache")
+json_representation = json.loads(program_input)
 bookmarks = json_representation['bookmarks']
 
 ## Apply transformations ##
 
 # Print first, unless supressed
 if should_print:
+    print((
+        'When invoked without options, this program prints a visual representation of the bookmark tree and then exits without applying any transformation. '
+        'When you are ready to transform the bookmark tree, specify the -d(elete) or -u(pdate) flags, followed by the index numbers of nodes you wish to modify. ' 
+        'Doing this will suppress printing the visual representation and will instead print the raw output tree to standard output for use by another program. '
+        '\n'
+        ))
     legibly_print(bookmarks)
+    # If we are simply legibly printing to see what we want to modify
+    # there is no need to output further in the pipeline.
+    sys.exit()
 
 # Delete nodes
 if args.delete:
-    deletion_range = list(range(len(bookmarks)))
-    print("deletion range is " + str(deletion_range))
-    valid_deletions = filter(lambda x: nodes_to_delete.index(x) not in deletion_range , nodes_to_delete)
-    new_bookmarks = filter(lambda x: bookmarks.index(x) not in nodes_to_delete , bookmarks)
-    pprint(list(new_bookmarks))
-# bookmarks = list(filter(keep_node, bookmarks))
+    # Invalid and repeated nodes are automatically ignored
+    bookmarks = list(filter(lambda x: bookmarks.index(x) not in nodes_to_delete, bookmarks))
+
+# TODO: implement a convenient range syntax: e.g., 1-5
+# probably needs to be handled in the argument parsing logic.
 
 # Update nodes
 # TODO: implement update logic
 
+# Indent nodes
+# TODO: implement indentation logic
+
+# Prepare for pdfcpu
+# expects a toplevel "bookmarks" key.
+bookmarks_tree = {"bookmarks": bookmarks}
+
+# Finally, output new node structure to sdout
+print(json.dumps(bookmarks_tree, ensure_ascii=False))
 
 
